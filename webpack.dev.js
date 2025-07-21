7e3fff0c85384e9cfdcf21b6f2dbd9c7df1cd17a
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-21 14:13:58 +0300

    Style header

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
- const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+// webpack.dev.js
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
- });
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true, // Add this for HMR
+  },
+});
